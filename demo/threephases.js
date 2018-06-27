ed7981de3c4275355b0e069c098c89667912d9df
Abiel Brhane <abielberaki1@gmail.com>
2018-06-27 14:46:50 -0600

    fix

--- demo/threephases.js
+++
@@ -1,5 +0,0 @@
-// https://nodejs.org/en/docs/guides/event-loop-timers-and-nexttick/
-
-setTimeout(() => { console.log('timeout'); }, 0);
-setImmediate(() => { console.log('immediate'); });
-process.nextTick(() => console.log('nexttick'));
